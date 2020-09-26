e7cbb5f5e3423792b550ec5e63a3709b1aa6b3d0
krassowski <krassowski.michal@gmail.com>
2020-09-26 22:26:25 +0100

    Fix a typo

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
             'Intended Audience :: Developers',
             'Intended Audience :: Science/Research',
             'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7'
+            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8'
         ],
         install_requires=['requests'],
